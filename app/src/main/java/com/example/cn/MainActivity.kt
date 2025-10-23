a5e3479a7200b541ed1f7655693f3e33f79ae75f
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-10-23 18:02:53 +0700

    mdbhh

--- app/src/main/java/com/example/cn/MainActivity.kt
+++ app/src/main/java/com/example/cn/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CnTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        //namw
+                        //name
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
