5331a4caa58a9284a3a9785c12a68044e53052f8
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-12 22:50:35 +0700

    test

--- app/src/main/java/com/example/cn/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/cn/view/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.cn.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
@@ -9,16 +10,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.cn.R
+
 
 @Composable
 fun WelcomeScreen(
     onSubmitClick: () -> Unit
 ) {
-
     val colorBackground = Color(0xFFE6E6FA)
     val colorPurple700 = Color(0xFF673AB7)
     val colorDeepPurple = Color(0xFF512DA8)
@@ -31,59 +35,32 @@ fun WelcomeScreen(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(top = 40.dp)
+            modifier = Modifier.padding(top = 60.dp)
         ) {
+            Spacer(modifier = Modifier.height(100.dp))
+
             Text(
                 text = "Selamat Datang",
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Normal,
+                fontSize = 37.sp,
+                fontWeight = FontWeight.Medium,
+                fontFamily = FontFamily.Monospace,
                 color = colorPurple700,
                 textAlign = TextAlign.Center
             )
         }
-
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.weight(1f),
             verticalArrangement = Arrangement.Center
         ) {
-            Text(
-                text = "CARD-LST",
-                fontSize = 36.sp,
-                fontWeight = FontWeight.Bold,
-                color = colorDeepPurple,
-                letterSpacing = 2.sp
-            )
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Text(
-                text = "🪐",
-                fontSize = 80.sp,
-                modifier = Modifier.padding(vertical = 16.dp)
-            )
-
-            Text(
-                text = "MOBILE APP",
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Normal,
-                color = colorPurple700,
-                letterSpacing = 1.sp
-            )
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            Text(
-                text = "2025",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = colorDeepPurple
+            Image(
+                painter = painterResource(id = R.drawable.logoumy), // pastikan file ada di res/drawable
+                contentDescription = "Logo",
+                modifier = Modifier.size(300.dp)
             )
         }
-
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(bottom = 60.dp)
@@ -102,15 +79,13 @@ fun WelcomeScreen(
                 color = colorPurple700,
                 modifier = Modifier.padding(bottom = 24.dp)
             )
-
             Button(
                 onClick = onSubmitClick,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 32.dp)
-                    .height(50.dp),
+                    .height(56.dp),
                 colors = ButtonDefaults.buttonColors(
-                    // Menggunakan val colorDeepPurple
                     containerColor = colorDeepPurple
                 )
             ) {
@@ -120,6 +95,7 @@ fun WelcomeScreen(
                     fontWeight = FontWeight.Bold
                 )
             }
+            Spacer(modifier = Modifier.height(32.dp))
         }
     }
 }
